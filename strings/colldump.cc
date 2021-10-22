ddc1b9cb84b64faaae746728a6967eb5894824a6
Vicent Marti <vmg@strn.cat>
2021-10-22 11:49:10 +0200

    colldump: generate JSON using rapidjson
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- strings/colldump.cc
+++ strings/colldump.cc
@@ -18,94 +18,156 @@
 #include "my_io.h"
 #include "my_loglevel.h"
 #include "my_macros.h"
-
 #include "str_uca_type.h"
 
-static void print_contractions_1(FILE *f, my_wc_t *buf, size_t depth, bool *first, bool contextual, const MY_CONTRACTION &contraction) {
-  buf[depth] = contraction.ch;
+#include "my_rapidjson_size_t.h"
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/writer.h"
+
+template<typename J>
+static void print_contractions_1(J &json, my_wc_t *path, size_t depth, bool contextual, const MY_CONTRACTION &contraction) {
+  path[depth] = contraction.ch;
 
   if (contraction.is_contraction_tail) {
-    if (!*first) {
-      fprintf(f, ",");
+    json.StartObject();
+
+    json.Key("Path");
+    json.StartArray();
+    for (size_t i = 0; i <= depth; i++) {
+      json.Uint((unsigned int)path[i]);
+    }
+    json.EndArray();
+
+    json.Key("Weights");
+    json.StartArray();
+    for (size_t i = 0; i < MY_UCA_MAX_WEIGHT_SIZE; i++) {
+      json.Uint(contraction.weight[i]);
+    }
+    json.EndArray();
+
+    if (contextual) {
+      json.Key("Contextual");
+      json.Bool(true);
     }
 
-    *first = false;
-    fprintf(f, "{\"Path\" : [");
-    for (size_t i = 0; i <= depth; i++) {
-      fprintf(f, "%s%d", i > 0 ? "," : "", (unsigned int)buf[i]);
-    }
-    fprintf(f, "], \"Weights\" : [");
-    for (size_t i = 0; i < MY_UCA_MAX_WEIGHT_SIZE; i++) {
-      fprintf(f, "%s%d", i > 0 ? "," : "", contraction.weight[i]);
-    }
-    fprintf(f, "]");
-    if (contextual) {
-      fprintf(f, ", \"Contextual\" : true");
-    }
-    fprintf(f, "}");
+    json.EndObject();
   }
 
   for (const MY_CONTRACTION &ctr : contraction.child_nodes) {
-    print_contractions_1(f, buf, depth+1, first, false, ctr);
+    print_contractions_1(json, path, depth+1, false, ctr);
   }
   for (const MY_CONTRACTION &ctr : contraction.child_nodes_context) {
-    print_contractions_1(f, buf, depth+1, first, true, ctr);
+    print_contractions_1(json, path, depth+1, true, ctr);
   }
 }
 
-static void print_contractions(FILE *f, std::vector<MY_CONTRACTION> *contractions) {
-  my_wc_t buf[256];
-  bool first = true;
-
+template<typename J>
+static void print_contractions(J &json, std::vector<MY_CONTRACTION> *contractions) {
+  my_wc_t path[256];
+  json.StartArray();
   for (const MY_CONTRACTION &ctr : *contractions) {
-    print_contractions_1(f, buf, 0, &first, false, ctr);
+    print_contractions_1(json, path, 0, false, ctr);
   }
+  json.EndArray();
 }
 
-static void print_reorder_params(FILE *f, struct Reorder_param *reorder) {
+template<typename J>
+static void print_reorder_params(J &json, struct Reorder_param *reorder) {
+  json.StartArray();
   for (int i = 0; i < reorder->wt_rec_num; i++) {
     struct Reorder_wt_rec &r = reorder->wt_rec[i];
-    fprintf(f, "%s[%d, %d, %d, %d]", i > 0 ? ", " : "",
-      r.old_wt_bdy.begin, r.old_wt_bdy.end,
-      r.new_wt_bdy.begin, r.new_wt_bdy.end);
+    json.StartArray();
+      json.Uint(r.old_wt_bdy.begin);
+      json.Uint(r.old_wt_bdy.end);
+      json.Uint(r.new_wt_bdy.begin);
+      json.Uint(r.new_wt_bdy.end);
+    json.EndArray();
   }
+  json.EndArray();
 }
 
-static void printWeightsForCodepoint(FILE *f, int codepoint, uint16 **weights) {
+template<typename J>
+static void print_unipages(J &json, const MY_UNI_IDX *unicodeidx) {
+  json.StartArray();
+  for (const MY_UNI_IDX *idx = unicodeidx; idx->tab != NULL; idx++) {
+    json.StartObject();
+    json.Key("From");
+    json.Uint(idx->from);
+    json.Key("To");
+    json.Uint(idx->to);
+    json.Key("Tab");
+    json.StartArray();
+    const size_t entries = idx->to - idx->from;
+    for (size_t i = 0; i <= entries; i++) {
+      json.Uint(idx->tab[i]);
+    }
+    json.EndArray();
+    json.EndObject();
+  }
+  json.EndArray();
+}
+
+template<typename J>
+static void print_uca_weights_900(J &json, int codepoint, uint16 **weights) {
   uint16 *page = weights[codepoint >> 8];
   if (page == NULL)
     return;
 
-  fprintf(f, "%s\"U+%04X\":[", codepoint > 0 ? ",\n\t\t" : "\n\t\t", codepoint);
-
   int offset = codepoint & 0xFF;
   int cecount = page[offset];
 
+  rapidjson::StringBuffer key;
+  rapidjson::UTF8<>::Encode(key, codepoint);
+
+  json.Key(key.GetString(), key.GetSize());
+  json.StartArray();
   for (int ce = 0; ce < cecount; ce++) {
-    int weight1 = page[256 + (ce*3+0)*256+offset];
-    int weight2 = page[256 + (ce*3+1)*256+offset];
-    int weight3 = page[256 + (ce*3+2)*256+offset];
-    fprintf(f, "%s[%d,%d,%d]", ce > 0 ? "," : "", weight1, weight2, weight3);
+    json.Uint(page[256 + (ce*3+0)*256+offset]);
+    json.Uint(page[256 + (ce*3+1)*256+offset]);
+    json.Uint(page[256 + (ce*3+2)*256+offset]);
   }
-  fprintf(f, "]");
+  json.EndArray();
 }
 
-static void print_array_file(const uchar *arr, size_t len, FILE *f) {
-  fprintf(f, "[");
-  for (size_t i = 0; i < len; ++i) {
-    fprintf(f, " %u", arr[i]);
-    fprintf(f, (i + 1 < len) ? "," : "");
+template<typename J>
+static void print_uca_weights_legacy(J &json, int codepoint, uint16 **weights, uchar *lengths) {
+  uint16 *page = weights[codepoint >> 8];
+  if (page == NULL)
+    return;
+
+  int offset = codepoint & 0xFF;
+  uint16 *w = page + offset * lengths[codepoint >> 8];
+  if (!w[0])
+    return;
+
+  rapidjson::StringBuffer key;
+  rapidjson::UTF8<>::Encode(key, codepoint);
+
+  json.Key(key.GetString(), key.GetSize());
+  json.StartArray();
+  for (; w[0]; w++) {
+    json.Uint(w[0]);
   }
-  fprintf(f, "]");
+  json.EndArray();
 }
 
-static void print_array16_file(const uint16 *arr, size_t len, FILE *f) {
-  fprintf(f, "[");
+template<typename J>
+static void print_array_uchar(J &json, const uchar *arr, size_t len) {
+  json.StartArray();
   for (size_t i = 0; i < len; ++i) {
-    fprintf(f, " %u", arr[i]);
-    fprintf(f, (i + 1 < len) ? "," : "");
+    json.Uint(arr[i]);
   }
-  fprintf(f, "]");
+  json.EndArray();
+}
+
+template<typename J>
+static void print_array_uint16(J &json, const uint16 *arr, size_t len) {
+  json.StartArray();
+  for (size_t i = 0; i < len; ++i) {
+    json.Uint(arr[i]);
+  }
+  json.EndArray();
 }
 
 static CHARSET_INFO *init_collation(const char *name) {
@@ -138,18 +200,156 @@ static KNOWN_HANDLER known_handlers[] = {
   { "ucs2_uca", &my_collation_ucs2_uca_handler },
 };
 
+static int dumpall(const char *dumppath) {
+  char pathbuf[4096];
+  char jsonbuf[4096*4];
+
+  // bootstrap the `all_charsets` collation array
+  init_collation("utf8mb4_0900_ai_ci");
+
+  for (const CHARSET_INFO *charset : all_charsets) {
+    if (!charset || (charset->state & MY_CS_AVAILABLE) == 0)
+      continue;
+
+    charset = init_collation(charset->name);
+    snprintf(pathbuf, sizeof(pathbuf), "%s/%s.json", dumppath, charset->name);
+
+    FILE *jsonfile = fopen(pathbuf, "w");
+    if (jsonfile == NULL) {
+      fprintf(stderr, "failed to create '%s'\n", pathbuf);
+      return 1;
+    }
+
+    rapidjson::FileWriteStream os(jsonfile, jsonbuf, sizeof(jsonbuf));
+    rapidjson::Writer< rapidjson::FileWriteStream, rapidjson::UTF8<>, rapidjson::ASCII<> > json(os);
+
+    json.StartObject();
+    json.Key("Name");
+    json.String(charset->name);
+    json.Key("Charset");
+    json.String(charset->csname);
+    json.Key("Binary");
+    json.Bool((charset->state & MY_CS_BINSORT) != 0);
+    json.Key("Number");
+    json.Uint(charset->number);
+
+    for (const KNOWN_HANDLER &handler : known_handlers) {
+      if (charset->coll == handler.h) {
+        json.Key("CollationImpl");
+        json.String(handler.name);
+        break;
+      }
+    }
+
+    if (charset->ctype != NULL) {
+      json.Key("CType");
+      print_array_uchar(json, charset->ctype, 256);
+    }
+
+    if (charset->to_lower != NULL) {
+      json.Key("ToLower");
+      print_array_uchar(json, charset->to_lower, 256);
+    }
+
+    if (charset->to_upper != NULL) {
+      json.Key("ToUpper");
+      print_array_uchar(json, charset->to_upper, 256);
+    }
+
+    if (charset->tab_to_uni != NULL) {
+      json.Key("TabToUni");
+      print_array_uint16(json, charset->tab_to_uni, 256);
+    }
+
+    if (charset->tab_from_uni != NULL) {
+      json.Key("TabFromUni");
+      print_unipages(json, charset->tab_from_uni);
+    }
+
+    if (charset->sort_order != NULL) {
+      json.Key("SortOrder");
+      print_array_uchar(json, charset->sort_order, 256);
+    }
+
+    if (charset->uca != NULL) {
+      MY_UCA_INFO *uca = charset->uca;
+
+      json.Key("UCAVersion");
+
+      switch (uca->version) {
+      case UCA_V400: json.Uint(400); break;
+      case UCA_V520: json.Uint(520); break;
+      case UCA_V900: json.Uint(900); break;
+      default: json.Uint(0); break;
+      }
+
+      json.Key("Weights");
+      json.StartObject();
+      if (uca->version == UCA_V900) {
+        for (my_wc_t cp = 0; cp < MY_UCA_MAXCHAR; cp++) {
+          print_uca_weights_900(json, cp, uca->weights);
+        }
+      } else {
+        for (my_wc_t cp = 0; cp < uca->maxchar; cp++) {
+          print_uca_weights_legacy(json, cp, uca->weights, uca->lengths);
+        }
+      }
+      json.EndObject();
+
+      if (uca->have_contractions) {
+        json.Key("Contractions");
+        print_contractions(json, uca->contraction_nodes);
+      }
+    }
+
+    if (charset->coll_param != NULL) {
+      json.Key("UppercaseFirst");
+      json.Bool(charset->coll_param->case_first == CASE_FIRST_UPPER);
+        
+      if (charset->coll_param->reorder_param != NULL) {
+        json.Key("Reorder");
+        print_reorder_params(json, charset->coll_param->reorder_param);
+      }
+    }
+
+    json.EndObject();
+    json.Flush();
+    fclose(jsonfile);
+  }
+  return 0;
+}
+
+static const uchar FORCE_INPUT[0] = {};
+
 int main(int argc, char **argv) {
-  if (argc == 3 && !strcmp(argv[1], "--test")) {
-    uchar src[4096];
-    uchar dst[4096*4];
+  if (argc >= 3 && !strcmp(argv[1], "--test")) {
+    char src[4096];
+    char dst[4096*4];
+    bool debug = argc >= 4 && !strcmp(argv[3], "--debug");
     CHARSET_INFO *cs = init_collation(argv[2]);
     if (cs == NULL) {
       fprintf(stderr, "unknown collation: %s", argv[2]);
       return 1;
     }
-    size_t readin = fread(src, 1, 4096, stdin);
-    size_t wlen = cs->coll->strnxfrm(cs, dst, sizeof(dst), 0, src, readin, 0);
-    fwrite(dst, 1, wlen, stdout);
+    size_t readin;
+    if (debug) {
+      memcpy(src, FORCE_INPUT, sizeof(FORCE_INPUT));
+      readin = sizeof(FORCE_INPUT);
+    } else {
+      readin = fread(src, 1, 4096, stdin);
+    }
+    size_t codepoints = cs->cset->numchars(cs, src, src+readin);
+    size_t wlen = cs->coll->strnxfrm(cs, (uchar *)dst, sizeof(dst), codepoints, (const uchar *)src, readin, 0);
+
+    if (debug) {
+      fprintf(stdout, "[");
+      for (size_t i = 0; i < wlen; i++) {
+        fprintf(stdout, "%s%d", i > 0 ? ", " : "", (uchar)dst[i]);
+      }
+      fprintf(stdout, "]\n");
+    } else {
+      fwrite(dst, 1, wlen, stdout);
+    }
     return 0;
   }
 
@@ -158,98 +358,5 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char pathbuf[4096];
-
-  // Load one collation to get everything going.
-  init_collation("utf8mb4_0900_ai_ci");
-  for (const CHARSET_INFO *cs : all_charsets) {
-    if (cs && (cs->state & MY_CS_AVAILABLE)) {
-      CHARSET_INFO *cs_loaded = init_collation(cs->name);
-      snprintf(pathbuf, sizeof(pathbuf), "%s/%s.json", argv[1], cs_loaded->name);
-
-      FILE *fl = fopen(pathbuf, "w");
-      if (fl == NULL) {
-        fprintf(stderr, "failed to create '%s'\n", pathbuf);
-        return 1;
-      }
-
-      fprintf(fl, "{\n");
-      fprintf(fl, "\t\"Name\": \"%s\",\n", cs_loaded->name);
-      fprintf(fl, "\t\"Charset\": \"%s\",\n", cs_loaded->csname);
-      fprintf(fl, "\t\"Binary\": %s,\n", (cs_loaded->state & MY_CS_BINSORT) ? "true" : "false");
-
-      for (const KNOWN_HANDLER &handler : known_handlers) {
-        if (cs_loaded->coll == handler.h) {
-          fprintf(fl, "\t\"CollationImpl\": \"%s\",\n", handler.name);
-          break;
-        }
-      }
-
-      fprintf(fl, "\t\"Number\": %u", cs_loaded->number);
-
-      if (cs->ctype != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"CType\": ");
-        print_array_file(cs_loaded->ctype, 256, fl);
-      }
-
-      if (cs->to_lower != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"ToLower\": ");
-        print_array_file(cs_loaded->to_lower, 256, fl);
-      }
-
-      if (cs->to_upper != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"ToUpper\": ");
-        print_array_file(cs_loaded->to_upper, 256, fl);
-      }
-
-      if (cs->tab_to_uni != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"TabToUni\": ");
-        print_array16_file(cs_loaded->tab_to_uni, 256, fl);
-      }
-
-      if (cs_loaded->sort_order != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"SortOrder\": ");
-        print_array_file(cs_loaded->sort_order, 256, fl);
-      }
-
-      if (cs_loaded->uca != NULL) {
-        fprintf(fl, ",\n");
-        fprintf(fl, "\t\"Weights\":{");
-        if (cs_loaded->uca->version == UCA_V900) {
-          for (int cp = 0; cp < MY_UCA_MAXCHAR; cp++) {
-            printWeightsForCodepoint(fl, cp, cs_loaded->uca->weights);
-          }
-        } else {
-          // TODO
-        }
-        fprintf(fl, "\n\t}");
-
-        if (cs_loaded->uca->have_contractions) {
-          fprintf(fl, ",\n");
-          fprintf(fl, "\t\"Contractions\":[\n");
-          print_contractions(fl, cs_loaded->uca->contraction_nodes);
-          fprintf(fl, "\n\t]");
-        }
-      }
-
-      if (cs_loaded->coll_param != NULL) {
-        fprintf(fl, ",\n\t\"UppercaseFirst\":%s",
-          cs_loaded->coll_param->case_first == CASE_FIRST_UPPER ? "true" : "false");
-        if (cs_loaded->coll_param->reorder_param != NULL) {
-          fprintf(fl, ",\n");
-          fprintf(fl, "\t\"Reorder\":[\n");
-          print_reorder_params(fl, cs_loaded->coll_param->reorder_param);
-          fprintf(fl, "\n\t]");
-        }
-      }
-
-      fprintf(fl, "\n}");
-      fclose(fl);
-    }
-  }
+  return dumpall(argv[1]);
 }
